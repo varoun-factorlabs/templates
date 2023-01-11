6953e638ba8edabde2bcbfaef78cb71c818eb2df
Varoun Hanooman <44379467+varounhanooman@users.noreply.github.com>
2023-01-11 05:53:07 +0000

    changed the structure and added nftsushi and navbar

--- main.py
+++ main.py
@@ -11,20 +11,34 @@ app = Flask('app')
 app.config['UPLOAD_FOLDER'] = RESULTS_FOLDER
 
 
-@app.route('/')
-def index():
+@app.route('/finsbeachbar')
+def finsbeachbar():
   remix = False
   return render_template("gallery.html", remix=remix)
 
-@app.route('/underwater')
+
+@app.route('/juicyju33')
 def underwater():
   remix = False
   return render_template("gallery2.html", remix=remix)
+
+
+@app.route('/NFT_Sushi')
+def pip():
+  remix = False
+  return render_template("gallery3.html", remix=remix)
+
 
 @app.route('/templates.xyz')
 def home():
   remix = False
   return render_template("index.html", remix=remix)
+
+
+@app.route('/')
+def circle():
+  remix = False
+  return render_template("circles.html", remix=remix)
 
 
 # @app.route('/compute')
@@ -37,7 +51,7 @@ def home():
 #   return render_template('gallery.html', remix=remix)
 
 
-@app.route('/result', methods=['POST', 'GET'])
+@app.route('/finsbeachbar', methods=['POST', 'GET'])
 def result():
   remix = True
   if request.method == 'POST':
@@ -46,8 +60,8 @@ def result():
     # if isinstance(Doodle, int) == False:
     #   raise ValueError("Number missing!")
     drink = request.form.get("drink")
-    proc_fins.compute("AAA.png", "BBB.png", "final_new.png",
-                      str(Doodle), drink)
+    proc_fins.compute("AAA.png", "BBB.png", "final_new.png", str(Doodle),
+                      drink)
     full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'final_new.png')
     AAA = os.path.join(app.config['UPLOAD_FOLDER'], 'AAA.png')
     BBB = os.path.join(app.config['UPLOAD_FOLDER'], 'BBB.png')
@@ -58,7 +72,8 @@ def result():
     #                        BBB_image=BBB,
     #                        dood_num=str(Doodle).zfill(4))
 
-@app.route('/result2', methods=['POST', 'GET'])
+
+@app.route('/juicyju33', methods=['POST', 'GET'])
 def result2():
   remix = True
   if request.method == 'POST':
@@ -67,12 +82,29 @@ def result2():
     # if isinstance(Doodle, int) == False:
     #   raise ValueError("Number missing!")
     drink = request.form.get("drink")
-    proc_undr.compute("AAA.png", "BBB.png", "final_new.png",
-                      str(Doodle), drink)
+    proc_undr.computeUnderwater("AAA.png", "BBB.png", "final_new.png",
+                                str(Doodle), drink)
     full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'final_new.png')
     AAA = os.path.join(app.config['UPLOAD_FOLDER'], 'AAA.png')
     BBB = os.path.join(app.config['UPLOAD_FOLDER'], 'BBB.png')
     return render_template("gallery2.html", remix=remix, dood_num=Doodle)
+
+
+@app.route('/NFT_Sushi', methods=['POST', 'GET'])
+def result3():
+  remix = True
+  if request.method == 'POST':
+    #result = request.form
+    Doodle = request.form.get("Doodle")
+    # if isinstance(Doodle, int) == False:
+    #   raise ValueError("Number missing!")
+    pip = request.form.get("pip")
+    proc_undr.computePip("AAA.png", "BBB.png", "final_new.png", str(Doodle),
+                         pip)
+    full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'final_new.png')
+    AAA = os.path.join(app.config['UPLOAD_FOLDER'], 'AAA.png')
+    BBB = os.path.join(app.config['UPLOAD_FOLDER'], 'BBB.png')
+    return render_template("gallery3.html", remix=remix, dood_num=Doodle)
 
 
 app.run(host='0.0.0.0', port=8080)
--- proc_undr.py
+++ proc_undr.py
@@ -1,6 +1,7 @@
 import numpy as np
 import sys
 from PIL import Image
+
 Image.LOAD_TRUNCATED_IMAGES = True
 import os
 import requests
@@ -10,8 +11,8 @@ import json
 #https://dood-collection-test.s3.amazonaws.com/8686.png
 
 
-def compute(INPUT_FG: str, INPUT_BG: str, OUTPUT: str, DOOD_TKN: str,
-            FG_TKN: str):
+def computeUnderwater(INPUT_FG: str, INPUT_BG: str, OUTPUT: str, DOOD_TKN: str,
+                      FG_TKN: str):
 
   image_URL = f'https://i.ibb.co/PZzzcjr/back.png'
 
@@ -91,3 +92,92 @@ def compute(INPUT_FG: str, INPUT_BG: str, OUTPUT: str, DOOD_TKN: str,
 
   # Save this image
   back_resized_img.save(f"static/results/underwater_doodle1.png", format="png")
+
+
+########################################################################
+################################  PIP   ################################
+########################################################################
+
+
+def computePip(INPUT_FG: str, INPUT_BG: str, OUTPUT: str, DOOD_TKN: str,
+               PIP_TKN: str):
+
+  # download doodle image
+  url = f"https://deep-index.moralis.io/api/v2/nft/0x8a90CAb2b38dba80c64b7734e58Ee1dB38B8992e/{DOOD_TKN}"
+
+  params = {
+    "chain": "eth",
+  }
+
+  headers = {
+    "X-API-KEY":
+    "Yj6gNd9QGdurZ3PXaAW8rRA5XSyl4wBb0qhSTOKUpXS5sml0Zvt6pKjubkgN7jXh"
+  }
+
+  response = requests.request("GET", url, headers=headers, params=params)
+
+  metadata = json.loads(response.text)
+
+  # print(DOOD_TKN)
+  # print(url)
+
+  image_metadata = metadata["metadata"]
+
+  image_metadata_json = json.loads(image_metadata)
+
+  image_hash = image_metadata_json["image"][7:]
+
+  #image_URL = f'https://ipfs.moralis.io:2053/ipfs/{image_hash}'
+  image_URL = f'https://gateway.moralisipfs.com/ipfs/{image_hash}'
+
+  print(image_URL)
+
+  #wget.download(image_URL)
+
+  img_data = requests.get(image_URL).content
+
+  with open('image_name.jpg', 'wb') as handler:
+    handler.write(img_data)
+
+  # download drink image
+
+  drink_img = requests.get(PIP_TKN).content
+
+  with open('pip.png', 'wb') as handler:
+    handler.write(drink_img)
+
+  # Back Image
+  filename = 'image_name.jpg'
+
+  # Mid Image
+  filename1 = f'pip.png'
+
+  # Open Back Image
+  backImage = Image.open(filename)
+
+  # Open Mid Image
+  midImage = Image.open(filename1)
+
+  # Convert image to RGBA
+  backImage = backImage.convert("RGBA")
+
+  # Convert image to RGBA
+  midImage = midImage.convert("RGBA")
+
+  #WIDTH = backImage.size[0]
+  #HEIGHT = backImage.size[0]
+
+  WIDTH = 800
+  HEIGHT = 800
+
+  resized_img = midImage.resize((WIDTH, HEIGHT))
+  resized_img.save("resized_image.png")
+
+  back_resized_img = backImage.resize((WIDTH, HEIGHT))
+  back_resized_img.save("back_resized_img.png")
+
+  # Paste the frontImage at (width, height)
+  back_resized_img.paste(resized_img, (0, 0), resized_img)
+
+  # Save this image
+  back_resized_img.save(f"static/results/pip_doodle1.png", format="png")
